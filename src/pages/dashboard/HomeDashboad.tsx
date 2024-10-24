08b295c2888a306e135740826f2e8360cde903bd
collinsadi <collinsadi20@gmail.com>
2024-10-24 12:22:23 +0100

    changes

--- src/pages/dashboard/HomeDashboad.tsx
+++ src/pages/dashboard/HomeDashboad.tsx
@@ -1,83 +1,93 @@
-import DashNav from "./DashNav"
-import { dashboardImages } from "../../utils/images"
+import DashNav from "./DashNav";
+import { dashboardImages } from "../../utils/images";
 import { GoArrowUpRight } from "react-icons/go";
-import { Modal, ModalCloseButton, ModalContent, ModalHeader, ModalOverlay, useDisclosure } from "@chakra-ui/react";
+import {
+  Modal,
+  ModalCloseButton,
+  ModalContent,
+  ModalHeader,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import { useState } from "react";
-import {useNavigate } from 'react-router-dom';
-import { useAccount, useReadContract } from 'wagmi'
-import abi from '../../config/abi.json'
+import { useNavigate } from "react-router-dom";
+import { useAccount, useReadContract } from "wagmi";
+import abi from "../../config/abi.json";
 import { CONTRACT_ADDRESS } from "../../config/contractAddress";
 const HomeDashboad = () => {
-
   const OverlayOne = () => (
     <ModalOverlay
-      bg='blackAlpha.300'
-      backdropFilter='blur(3px) hue-rotate(0deg)'
+      bg="blackAlpha.300"
+      backdropFilter="blur(3px) hue-rotate(0deg)"
     />
-  )
+  );
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [overlay, setOverlay] = useState(<OverlayOne />);
 
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [overlay, setOverlay] = useState(<OverlayOne />)
+  const [itemName, setItemName] = useState<string>("");
+  const [amount, setAmount] = useState<string>("");
+  const [currency, setCurrency] = useState<string>("");
+  const [initiatorRole, setInitiatorRole] = useState<string>();
 
-  const [itemName, setItemName] = useState<string>('')
-  const [amount, setAmount] = useState<string>('')
-  const [currency, setCurrency] = useState<string>('')
-  const [initiatorRole, setInitiatorRole] = useState<string>()
-
-  const [isBuyer, setIsBuyer] = useState(false)
-  const [isSeller, setIsSeller] = useState(false)
+  const [isBuyer, setIsBuyer] = useState(false);
+  const [isSeller, setIsSeller] = useState(false);
 
   const navigate = useNavigate();
 
   const handleBuyerClick = () => {
-    setIsBuyer(true)
-    setIsSeller(false)
-    setInitiatorRole("buyer")
-  }
+    setIsBuyer(true);
+    setIsSeller(false);
+    setInitiatorRole("buyer");
+  };
 
   const handleSellerClick = () => {
-    setIsSeller(true)
-    setIsBuyer(false)
-    setInitiatorRole("seller")
-  }
+    setIsSeller(true);
+    setIsBuyer(false);
+    setInitiatorRole("seller");
+  };
 
+  const handleItemName = (e: any) => {
+    setItemName(e.target.value);
+  };
 
-  const handleItemName = (e : any)=>{
-    setItemName(e.target.value)
-  }
+  const handleAmount = (e: any) => {
+    setAmount(e.target.value);
+  };
 
-  const handleAmount = (e : any)=>{
-    setAmount(e.target.value)
-  }
+  const handleCurrency = (e: any) => {
+    setCurrency(e.target.value);
+  };
 
-  const handleCurrency = (e : any)=>{
-    setCurrency(e.target.value)
-  }
+  const handleContinue = () => {
+    navigate(
+      `/create-transaction?itemName=${itemName}&amount=${amount}&currency=${currency}&initiator=${initiatorRole}`
+    );
+  };
 
-  const handleContinue = ()=>{
-    navigate(`/create-transaction?itemName=${itemName}&amount=${amount}&currency=${currency}&initiator=${initiatorRole}`);
-  }
-
-  const isFormValid = (isBuyer !== false || isSeller !== false) && itemName !== "" && amount !== "" && currency !== "" && currency !== "Select Pay";  
-
+  const isFormValid =
+    (isBuyer !== false || isSeller !== false) &&
+    itemName !== "" &&
+    amount !== "" &&
+    currency !== "" &&
+    currency !== "Select Pay";
 
   // ================= CONTRACT READING ====================
 
-  const {address} = useAccount()
-  
+  const { address } = useAccount();
+
   const { data, error, isPending } = useReadContract({
     abi,
     address: CONTRACT_ADDRESS,
-    functionName: 'getAllTransactionsForUser',
+    functionName: "getAllTransactionsForUser",
     account: address,
-  })
+  });
 
+  console.log("All transactions", data);
+  console.log("Error", error);
 
-  console.log('All transactions', data);
-  console.log('Error', error);
-  
-
+  // Data is an array of all the Ids of the transactions initiated by the user
+  // we can get their details buy running a loop on all the IDs and calling this function,  "getTransactionById (id)"
 
   return (
     <div>
@@ -85,53 +95,80 @@ const HomeDashboad = () => {
       <div className="lg:px-[15rem] px-3">
         <div className="flex justify-center items-center text-center h-screen">
           <div className="w-full">
-            <img src={dashboardImages.empty1} alt="" className="w-24 flex m-auto justify-center pb-5"/>
+            <img
+              src={dashboardImages.empty1}
+              alt=""
+              className="w-24 flex m-auto justify-center pb-5"
+            />
             <h2>No transactions yet</h2>
-            <button 
+            <button
               onClick={() => {
-                setOverlay(<OverlayOne />)
-                onOpen()
+                setOverlay(<OverlayOne />);
+                onOpen();
               }}
-              className="bg-[#054FBB] hover:bg-blue-600 flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md mt-5">
+              className="bg-[#054FBB] hover:bg-blue-600 flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md mt-5"
+            >
               Start New Transaction <GoArrowUpRight />
             </button>
 
-            {isPending && 'Loading . . .'}
+            {isPending && "Loading . . ."}
           </div>
         </div>
 
-        <Modal isCentered isOpen={isOpen} onClose={onClose} size={'2xl'} >
+        <Modal isCentered isOpen={isOpen} onClose={onClose} size={"2xl"}>
           {overlay}
-          <ModalContent maxW={{ base: '95vw', md: '600px' }} borderRadius={'10px'}>
+          <ModalContent
+            maxW={{ base: "95vw", md: "600px" }}
+            borderRadius={"10px"}
+          >
             <ModalHeader>Start Transaction Here!! </ModalHeader>
             <ModalCloseButton />
-            
+
             <form action="" className="lg:p-10 p-5 pt-5 ">
-              
               <div className="pb-3">
                 <p className="text-sm pb-3">Choose category (Buyer / Seller)</p>
                 <div className="flex items-center gap-3">
-                  <p onClick={handleBuyerClick} className={`${isBuyer === true ? 'bg-[#054FBB] w-fit hover:bg-blue-600  text-white' : 'border border-neutral-300'} 
-                    gap-3 cursor-pointer py-2 px-6 text-sm text-blck  rounded-md flex items-center `}>
+                  <p
+                    onClick={handleBuyerClick}
+                    className={`${
+                      isBuyer === true
+                        ? "bg-[#054FBB] w-fit hover:bg-blue-600  text-white"
+                        : "border border-neutral-300"
+                    } 
+                    gap-3 cursor-pointer py-2 px-6 text-sm text-blck  rounded-md flex items-center `}
+                  >
                     Buyer <GoArrowUpRight />
                   </p>
 
-                  <p onClick={handleSellerClick} className={`${isSeller === true ? 'bg-[#054FBB] w-fit hover:bg-blue-600  text-white' : 'border border-neutral-300'} 
-                    gap-3 cursor-pointer py-2 px-6 text-sm text-blck rounded-md flex items-center`}>
+                  <p
+                    onClick={handleSellerClick}
+                    className={`${
+                      isSeller === true
+                        ? "bg-[#054FBB] w-fit hover:bg-blue-600  text-white"
+                        : "border border-neutral-300"
+                    } 
+                    gap-3 cursor-pointer py-2 px-6 text-sm text-blck rounded-md flex items-center`}
+                  >
                     Seller <GoArrowUpRight />
                   </p>
                 </div>
-                {isBuyer === false && isSeller === false && 
-                  <p className="text-red-500 text-sm pt-3">Please select a role either buyer/seller</p>
-                }
+                {isBuyer === false && isSeller === false && (
+                  <p className="text-red-500 text-sm pt-3">
+                    Please select a role either buyer/seller
+                  </p>
+                )}
               </div>
 
               <div className="flex flex-col gap-7 pt-5">
                 <div>
-                  <p className="text-sm pb-2">What item are you buying/selling?</p>
-                  <input type="text" required 
-                    placeholder="Item name" 
-                    className="input text-sm input-bordered w-full" 
+                  <p className="text-sm pb-2">
+                    What item are you buying/selling?
+                  </p>
+                  <input
+                    type="text"
+                    required
+                    placeholder="Item name"
+                    className="input text-sm input-bordered w-full"
                     value={itemName}
                     onChange={handleItemName}
                   />
@@ -139,9 +176,11 @@ const HomeDashboad = () => {
 
                 <div>
                   <p className="text-sm pb-2">How much does the item cost? </p>
-                  <input type="number" required 
-                    placeholder="Amount" 
-                    className="input text-sm input-bordered w-full" 
+                  <input
+                    type="number"
+                    required
+                    placeholder="Amount"
+                    className="input text-sm input-bordered w-full"
                     value={amount}
                     onChange={handleAmount}
                   />
@@ -149,30 +188,32 @@ const HomeDashboad = () => {
 
                 <div>
                   <p className="text-sm pb-2">Select currency for payment</p>
-                  <select  
+                  <select
                     className="select select-bordered w-full text-sm"
                     value={currency}
                     onChange={handleCurrency}
                   >
                     <option>Select Pay</option>
-                    <option selected value={'USDT'}>USDT</option>
+                    <option selected value={"USDT"}>
+                      USDT
+                    </option>
                   </select>
                 </div>
 
-                {isFormValid ?
-                
-                  <button onClick={handleContinue}  
-                    className={`bg-[#054FBB] hover:bg-blue-600 w-full  flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md `}>
+                {isFormValid ? (
+                  <button
+                    onClick={handleContinue}
+                    className={`bg-[#054FBB] hover:bg-blue-600 w-full  flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md `}
+                  >
                     Get Started <GoArrowUpRight />
                   </button>
-                : 
-                
-                  <p  
-                    className={`bg-neutral-500 cursor-not-allowed w-full  flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md `}>
+                ) : (
+                  <p
+                    className={`bg-neutral-500 cursor-not-allowed w-full  flex items-center m-auto justify-center gap-3 py-3 px-6 text-sm text-white rounded-md `}
+                  >
                     Get Started <GoArrowUpRight />
                   </p>
-                }
-
+                )}
 
                 {/* </Link> */}
               </div>
@@ -181,7 +222,7 @@ const HomeDashboad = () => {
         </Modal>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeDashboad
+export default HomeDashboad;
